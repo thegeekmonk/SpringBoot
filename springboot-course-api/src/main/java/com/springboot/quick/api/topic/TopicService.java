f8262bfb89adb51d1e4f9995e861751b811c3d3b
Akhilesh Kumar <kumarakhilesh5441@gmail.com>
2022-01-05 13:21:27 +0530

    1st with controller,service new resource

--- springboot-course-api/src/main/java/com/springboot/quick/api/topic/TopicService.java
+++ springboot-course-api/src/main/java/com/springboot/quick/api/topic/TopicService.java
@@ -1,5 +1,6 @@
 package com.springboot.quick.api.topic;
 
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 
@@ -8,11 +9,11 @@ import org.springframework.stereotype.Service;
 @Service
 public class TopicService {
 
-	private List<Topic> topics = Arrays.asList(
+	private List<Topic> topics = new ArrayList<>(Arrays.asList(
 			new Topic("Java","Java Course","Java Description"),
 			new Topic("Spring","Java Course","Java Description"),
 			new Topic("Hibernate","Java Course","Java Description")
-			);
+			));
 	
 	public TopicService() {
 		// TODO Auto-generated constructor stub
@@ -26,6 +27,11 @@ public class TopicService {
 	public Topic getTopic(String id) {
 		// TODO Auto-generated method stub
 		return topics.stream().filter(t->t.getId().equals(id)).findFirst().get();
+	}
+
+	public void setTopic(Topic topic) {
+		// TODO Auto-generated method stub
+		 topics.add(topic);
 	}
 
 }
